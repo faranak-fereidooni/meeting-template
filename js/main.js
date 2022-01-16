f943e8f0e7d0bdb39f80b03f45fef0662b676f0c
Faranak Fereidouni <faranak.fereidooni@gmail.com>
2022-01-16 15:13:18 +0100

    Build course section

--- js/main.js
+++ js/main.js
@@ -1,72 +1,105 @@
-
-/* click on navbar for mobile */ 
-function toggleMainMenu(){
-  const  xx=document.querySelector(".navbar");
+/* click on navbar for mobile */
+function toggleMainMenu() {
+  const xx = document.querySelector(".navbar");
   xx.classList.toggle("collapsible--expanded");
 }
 
-function toggleSubmenu(){
-  const  xx=document.querySelector(".navbar__submenue");
+function toggleSubmenu() {
+  const xx = document.querySelector(".navbar__submenue");
   xx.classList.toggle("page-submenu");
 }
 
-/* menue-icon for mobile */  
-const icons = document.querySelectorAll('.icon');
-icons.forEach (icon => {  
-  icon.addEventListener('click', (event) => {
+/* menue-icon for mobile */
+const icons = document.querySelectorAll(".icon");
+icons.forEach((icon) => {
+  icon.addEventListener("click", (event) => {
     icon.classList.toggle("open");
   });
 });
 
-/* ********************************************************************** */ 
+/* ********************************************************************** */
 /* click chevron-right */
-function togglecontent(){
-  const  xx=document.querySelector(".apply-now3");
+function togglecontent() {
+  const xx = document.querySelector(".apply-now3");
   xx.classList.toggle("collapsible--expanded");
 }
 
-function toggleSubcontent(){
-  const  xx=document.querySelector(".icon__container");
+function toggleSubcontent() {
+  const xx = document.querySelector(".icon__container");
   xx.classList.toggle("collapsible__content");
 }
 
-/* ********************************************************************** */ 
-/* slider */ 
-$(document).ready(function(){
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  responsiveClass:true,
-  margin:30,
-  responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        400:{
-          items:1,
-          nav:false
+/* ********************************************************************** */
+/* owl-carousel-service */
+$(document).ready(function () {
+  $(".owl-carousel-service").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 30,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-       768:{
-           items:2,
-           nav:false
+      400: {
+        items: 1,
+        nav: false,
+      },
+      768: {
+        items: 2,
+        nav: false,
+      },
+      820: {
+        items: 2,
+        nav: false,
+      },
+      912: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
     },
-       820:{
-           items:2,
-           nav:false
-  },
-      912:{
-          items:2,
-          nav:false
-},
-        1000:{
-            items:3,
-            nav:true,
-            loop:true
-        }
-    }
-})
-});
+  });
 
+  /* owl-carousel-course */
+  $(".owl-carousel-course").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 1,
+        nav: false,
+      },
+      768: {
+        items: 2,
+        nav: false,
+      },
+      820: {
+        items: 2,
+        nav: false,
+      },
+      912: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+});
 
 /* Collapsible */
 const collapsibles = document.querySelectorAll(".collapsible");
@@ -74,4 +107,4 @@ collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
-);   
+);
